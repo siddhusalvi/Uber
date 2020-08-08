799a5bbd7737aba26c0d5866bf8bad6214c7706f
siddhusalvi <siddheshsalvi525@gmail.com>
2020-08-08 13:58:43 +0530

    SOLVED : Firebase Error

--- app/src/main/java/com/siddhu/uber/CustomerLoginActivity.java
+++ app/src/main/java/com/siddhu/uber/CustomerLoginActivity.java
@@ -1,29 +1,24 @@
 package com.siddhu.uber;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class DriverLoginActivity extends AppCompatActivity {
+public class CustomerLoginActivity extends AppCompatActivity {
 
     private EditText mEmail,mPassword;
     private Button mlogin,mRegistration;
@@ -32,7 +27,7 @@ public class DriverLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_driver_login);
+        setContentView(R.layout.activity_customer_login);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -41,7 +36,7 @@ public class DriverLoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if(user != null){
-                    Intent intent = new Intent(DriverLoginActivity.this,MapActivity.class);
+                    Intent intent = new Intent(CustomerLoginActivity.this,MapActivity.class);
                     startActivity(intent);
                     finish();
                     return;
@@ -58,23 +53,16 @@ public class DriverLoginActivity extends AppCompatActivity {
         mRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Registration",Toast.LENGTH_SHORT).show();
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
-                Toast.makeText(DriverLoginActivity.this,email +" "+password,Toast.LENGTH_SHORT).show();
-                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(!task.isSuccessful()){
-                            if(!task.isSuccessful()){
-                                FirebaseAuthException e = (FirebaseAuthException )task.getException();
-                                Toast.makeText(DriverLoginActivity.this, "Failed Registration: "+e.getMessage(), Toast.LENGTH_SHORT).show();
-                                return;
-                            }
+                            Toast.makeText(getApplicationContext(),"sign up error",Toast.LENGTH_SHORT).show();
                         }else{
-                            Toast.makeText(getApplicationContext(),"Task is successful",Toast.LENGTH_SHORT).show();
                             String user_id = mAuth.getCurrentUser().getUid();
-                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(user_id);
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(user_id);
                             current_user_db.setValue(true);
                         }
                     }
@@ -85,9 +73,10 @@ public class DriverLoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
-                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(!task.isSuccessful()){
@@ -111,9 +100,4 @@ public class DriverLoginActivity extends AppCompatActivity {
         super.onStop();
         mAuth.removeAuthStateListener(firebaseAuthListener);
     }
-
-    public void T(String msg){
-        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT).show();
-    }
 }
-
--- app/src/main/java/com/siddhu/uber/MainActivity.java
+++ app/src/main/java/com/siddhu/uber/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +23,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,DriverLoginActivity.class);
+                Toast.makeText(getApplicationContext(),"Starting new activity",Toast.LENGTH_SHORT).show();
                 startActivity(intent);
-                finish();
-                return;
+//                finish();
+//                return;
             }
         });
 
+        mCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CustomerLoginActivity.class);
+                startActivity(intent);
+//                finish();
+//                return;
+
+            }
+        });
 
 
     }
